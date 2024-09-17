ecbf96797cd248d6dc36ec9ab298f7a9432a8dc2
Matheus Artur <matheusartur1@hotmail.com>
2024-09-17 16:36:45 -0300

    Preparando o ambiente de desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import './App.css'
-import SayMyName from './components/SayMyName'
-import Pessoa from './components/Pessoa'
-import Frase from './components/Frase'
-
 function App() {
-
-  const nome = "Maria"
-
   return (
     <div className="App">
-      <h1>Testando CSS</h1>
-      <Frase />
-      <Frase />
-      <SayMyName nome="Matheus" />
-      <SayMyName nome="João" />
-      <SayMyName nome={nome} />
-      <Pessoa
-        nome="Rodrigo"
-        idade="28"
-        profissao="Programador"
-        foto="https://via.placeholder.com/150"
-      />
+      <p>Home</p>
     </div>
   )
 }
 
-export default App 
+export default App
